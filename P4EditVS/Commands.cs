0c3b00a8d320437c0351eaed55f13aad33791a53
Scott Simpson <Scott.Simpson@WEASEL-SSIMPSON.weaseltron.loc>
2020-06-09 10:49:37 +0100

    initial commit of first version

---
+++ P4EditVS/Commands.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+using EnvDTE;
+
+namespace P4EditVS
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class Commands
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CheckoutCommandId = 0x0100;
+        public const int RevertIfUnchangedCommandId = 0x0101;
+        public const int RevertCommandId = 0x0103;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("bcd15dfb-5150-4bde-a3d0-520343ba88c5");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        private string activeFile;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Commands"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private Commands(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            // Checkout
+            {
+                var menuCommandID = new CommandID(CommandSet, CheckoutCommandId);
+                var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+                menuItem.BeforeQueryStatus += new EventHandler(OnBeforeQueryStatus);
+                commandService.AddCommand(menuItem);
+            }
+            // Revert if unchanged
+            {
+                var menuCommandID = new CommandID(CommandSet, RevertIfUnchangedCommandId);
+                var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+                menuItem.BeforeQueryStatus += new EventHandler(OnBeforeQueryStatus);
+                menuItem.Text = "Revert If Unchanged";
+                menuItem.Visible = true;
+                commandService.AddCommand(menuItem);
+            }
+            // Revert
+            {
+                var menuCommandID = new CommandID(CommandSet, RevertCommandId);
+                var menuItem = new OleMenuCommand(this.Execute, menuCommandID);
+                menuItem.BeforeQueryStatus += new EventHandler(OnBeforeQueryStatus);
+                menuItem.Text = "Revert";
+                menuItem.Visible = true;
+                commandService.AddCommand(menuItem);
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static Commands Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProviderAsync
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        private IServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CheckoutCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new Commands(package, commandService);
+        }
+
+        /// <summary>
+        /// Called before menu button is shown so we can update text and active state
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnBeforeQueryStatus(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            var myCommand = sender as OleMenuCommand;
+            if (null != myCommand)
+            {
+                EnvDTE80.DTE2 applicationObject = ServiceProvider.GetService(typeof(DTE)) as EnvDTE80.DTE2; 
+                if (applicationObject != null)
+                {
+                    // Cache the file name here so it doesn't change between now and executing the command (if that can even happen)
+                    activeFile = applicationObject.ActiveDocument.FullName;
+
+                    switch (myCommand.CommandID.ID)
+                    {
+                        case CheckoutCommandId:
+                            {
+                                myCommand.Text = string.Format("Checkout {0}", applicationObject.ActiveDocument.Name);
+                                myCommand.Enabled = applicationObject.ActiveDocument.ReadOnly;
+                            }
+                            break;
+                        case RevertIfUnchangedCommandId:
+                            {
+                                myCommand.Text = string.Format("Revert If Unchanged {0}", applicationObject.ActiveDocument.Name);
+                                myCommand.Enabled = !applicationObject.ActiveDocument.ReadOnly;
+                            }
+                            break;
+                        case RevertCommandId:
+                            {
+                                myCommand.Text = string.Format("Revert {0}", applicationObject.ActiveDocument.Name);
+                                myCommand.Enabled = !applicationObject.ActiveDocument.ReadOnly;
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            P4EditVS package = this.package as P4EditVS;
+            if(!package.ValidateUserSettings())
+            {
+                return;
+            }
+
+            var myCommand = sender as OleMenuCommand;
+
+            string globalOptions = package.GetGlobalP4CmdLineOptions();
+
+            EnvDTE80.DTE2 applicationObject = ServiceProvider.GetService(typeof(DTE)) as EnvDTE80.DTE2;
+            if (applicationObject != null)
+            {
+                string commandline = "";
+
+                switch (myCommand.CommandID.ID)
+                {
+                    case CheckoutCommandId:
+                        {
+                            commandline = string.Format("p4 {0} edit -c default {1}", globalOptions, activeFile);
+                        }
+                        break;
+                    case RevertIfUnchangedCommandId:
+                        {
+                            commandline = string.Format("p4 {0} revert -a {1}", globalOptions, activeFile);
+                        }
+                        break;
+                    case RevertCommandId:
+                        {
+                            IVsUIShell uiShell = ServiceProvider.GetService(typeof(SVsUIShell)) as IVsUIShell;
+
+                            bool shouldRevert = VsShellUtilities.PromptYesNo(
+                                "Are you sure you wish to revert and discard changes?",
+                                "P4EditVS: Revert",
+                                OLEMSGICON.OLEMSGICON_WARNING,
+                                uiShell);
+
+                            if(shouldRevert)
+                            {
+                                commandline = string.Format("p4 {0} revert {1}", globalOptions, activeFile);
+                            }
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                if (commandline != "")
+                {
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Arguments = "/C " + commandline;
+                    process.StartInfo = startInfo;
+                    process.Start();
+                }
+            }
+        }
+    }
+}
---
+++ P4EditVS/P4EditVS.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using Task = System.Threading.Tasks.Task;
+using System.ComponentModel;
+
+namespace P4EditVS
+{
+    /// <summary>
+    /// This is the class that implements the package exposed by this assembly.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The minimum requirement for a class to be considered a valid package for Visual Studio
+    /// is to implement the IVsPackage interface and register itself with the shell.
+    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
+    /// to do it: it derives from the Package class that provides the implementation of the
+    /// IVsPackage interface and uses the registration attributes defined in the framework to
+    /// register itself and its components with the shell. These attributes tell the pkgdef creation
+    /// utility what data to put into .pkgdef file.
+    /// </para>
+    /// <para>
+    /// To get loaded into VS, the package must be referred by &lt;Asset Type="Microsoft.VisualStudio.VsPackage" ...&gt; in .vsixmanifest file.
+    /// </para>
+    /// </remarks>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    //[ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExists_string, PackageAutoLoadFlags.BackgroundLoad)]
+    [Guid(P4EditVS.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideOptionPage(typeof(OptionPageGrid), "P4EditVS", "Settings", 0, 0, true)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class P4EditVS : AsyncPackage
+    {
+        /// <summary>
+        /// P4EditVS GUID string.
+        /// </summary>
+        public const string PackageGuidString = "d6a4db63-698d-4d16-bbc0-944fe52f83db";
+
+       //public string ServerAddress
+       //{
+       //    get
+       //    {
+       //        OptionPageGrid page = (OptionPageGrid)GetDialogPage(typeof(OptionPageGrid));
+       //        return page.ServerAddress;
+       //    }
+       //}
+
+        public string ClientName
+        {
+            get
+            {
+                OptionPageGrid page = (OptionPageGrid)GetDialogPage(typeof(OptionPageGrid));
+                return page.ClientName;
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                OptionPageGrid page = (OptionPageGrid)GetDialogPage(typeof(OptionPageGrid));
+                return page.UserName;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="P4EditVS"/> class.
+        /// </summary>
+        public P4EditVS()
+        {
+            // Inside this method you can place any initialization code that does not require
+            // any Visual Studio service because at this point the package object is created but
+            // not sited yet inside Visual Studio environment. The place to do all the other
+            // initialization is the Initialize method.
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        /// <param name="cancellationToken">A cancellation token to monitor for initialization cancellation, which can occur when VS is shutting down.</param>
+        /// <param name="progress">A provider for progress updates.</param>
+        /// <returns>A task representing the async work of package initialization, or an already completed task if there is none. Do not return null from this method.</returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await Commands.InitializeAsync(this);
+        }
+
+        public string GetGlobalP4CmdLineOptions()
+        {
+            return string.Format("-c {0} -u {1}", ClientName, UserName);
+        }
+
+        public bool ValidateUserSettings()
+        {
+            if(ClientName == "")
+            {
+                VsShellUtilities.ShowMessageBox(
+                this,
+                "Client name is empty. This must be set under Tools->Options->P4EditVS->Settings",
+                "Invalid Settings",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+                return false;
+            }
+
+            if (UserName == "")
+            {
+                VsShellUtilities.ShowMessageBox(
+                this,
+                "User name is empty. This must be set under Tools->Options->P4EditVS->Settings",
+                "Invalid Settings",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        #endregion
+    }
+
+    public class OptionPageGrid : DialogPage
+    {
+       // private string mServerAddress = "";
+       //
+       // [Category("Perforce Settings")]
+       // [DisplayName("Perforce Server Address")]
+       // [Description("Address of Perforce server. E.g. 127.0.0.1:1666")]
+       // public string ServerAddress
+       // {
+       //     get { return mServerAddress; }
+       //     set { mServerAddress = value; }
+       // }
+
+        private string mUserName = "";
+
+        [Category("Perforce Settings")]
+        [DisplayName("Perforce User Name")]
+        [Description("User name")]
+        public string UserName
+        {
+            get { return mUserName; }
+            set { mUserName = value; }
+        }
+
+        private string mClientName = "";
+
+        [Category("Perforce Settings")]
+        [DisplayName("Perforce Client Name")]
+        [Description("Client name, i.e. workspace name")]
+        public string ClientName
+        {
+            get { return mClientName; }
+            set { mClientName = value; }
+        }
+    }
+
+
+}
